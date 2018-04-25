60aa198c6ac017fba1a3b28b9c82a73ecb25162e
Alex Sullivan <alexsullivan114@gmail.com>
2018-04-25 18:18:51 -0400

    Make chat messages private

--- lib/chat/chat_list_screen.dart
+++ lib/chat/chat_list_screen.dart
@@ -2,6 +2,7 @@ import 'package:chatty_chat_chat/chat/chat_message.dart';
 import 'package:chatty_chat_chat/chat/chat_message_item_widget.dart';
 import 'package:chatty_chat_chat/chat/messaging.dart';
 import 'package:chatty_chat_chat/users.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/material.dart';
 
 class ChatListWidget extends StatefulWidget {
@@ -14,29 +15,32 @@ class ChatListWidget extends StatefulWidget {
 
 class _ChatWidgetState extends State<ChatListWidget> {
 
-  List<ChatMessage> messages = [];
+  List<ChatMessage> _messages = [];
 
   _ChatWidgetState() {
     subscribeToMessages().listen((chatMessage) {
       setState(() {
-        messages.add(chatMessage);
+        _messages.add(chatMessage);
       });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return new ListView.builder(itemBuilder: (context, index) {
-      if (index < messages.length) {
-        return _buildListItem(messages[index]);
-      } else {
-        return null;
-      }
-    });
+    return new ListView.builder(
+        itemBuilder: (context, index) {
+          if (index < _messages.length) {
+            return _buildListItem(_messages[index]);
+          } else {
+            return null;
+          }
+        });
   }
 
   Widget _buildListItem(ChatMessage chatMessage) {
-    MainAxisAlignment alignment = chatMessage.user == User.Puntu ? MainAxisAlignment.start : MainAxisAlignment.end;
+    MainAxisAlignment alignment = chatMessage.user == User.Puntu
+        ? MainAxisAlignment.start
+        : MainAxisAlignment.end;
     return new Row(
         mainAxisAlignment: alignment,
         children: <Widget>[
