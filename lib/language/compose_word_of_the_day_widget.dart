d04728976a1ce12d209c5c5da9d7aa37e995b19c
Alex Sullivan <Alexsullivan114@gmail.com>
2018-05-13 13:46:38 -0400

    Finish up word of the day feature

--- lib/language/compose_word_of_the_day_widget.dart
+++ lib/language/compose_word_of_the_day_widget.dart
@@ -1,23 +1,50 @@
+import 'package:chatty_chat_chat/language/word_of_the_day.dart';
+import 'package:chatty_chat_chat/language/word_of_the_day_streams.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class ComposeWordOfTheDayWidget extends StatelessWidget {
+  final TextEditingController _nepaliController = new TextEditingController();
+  final TextEditingController _englishController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return new Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
-        new Column(
-          children: <Widget>[
-            new Flexible(child: TextField()),
-            new Flexible(child: TextField())
-          ],
+        new Flexible(
+          child: new Container(
+              child: new Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  new Flexible(
+                    child: TextField(
+                      decoration:
+                          new InputDecoration(hintText: "Put dat Nepali here"),
+                      controller: _nepaliController,
+                    ),
+                    fit: FlexFit.loose,
+                  ),
+                  new Flexible(
+                    child: TextField(
+                      decoration:
+                          new InputDecoration(hintText: "Put dat english hurr"),
+                      controller: _englishController,
+                    ),
+                    fit: FlexFit.loose,
+                  ),
+                ],
+              ),
+              padding: new EdgeInsets.all(8.0)),
         ),
-        new FloatingActionButton(onPressed: (){
-
-        },
-        mini: true,
-        child: new Icon(Icons.send))
+        new FloatingActionButton(
+            onPressed: () {
+              addWordOfTheDay(new WordOfTheDay(
+                  _nepaliController.text, _englishController.text));
+            },
+            mini: true,
+            child: new Icon(Icons.send))
       ],
     );
   }
--- lib/language/word_of_the_day_widget.dart
+++ lib/language/word_of_the_day_widget.dart
@@ -4,7 +4,6 @@ import 'package:chatty_chat_chat/language/word_of_the_day_streams.dart';
 import 'package:flutter/widgets.dart';
 
 class WordOfTheDayWidget extends StatefulWidget {
-
   @override
   _WordOfTheDayState createState() {
     return new _WordOfTheDayState();
@@ -12,7 +11,6 @@ class WordOfTheDayWidget extends StatefulWidget {
 }
 
 class _WordOfTheDayState extends State<WordOfTheDayWidget> {
-
   List<WordOfTheDay> _words = [];
 
   _WordOfTheDayState() {
@@ -27,18 +25,22 @@ class _WordOfTheDayState extends State<WordOfTheDayWidget> {
   Widget build(BuildContext context) {
     String nepali = (_words.isEmpty) ? "" : _words.last.nepaliText;
     String english = (_words.isEmpty) ? "" : _words.last.englishText;
-    return new Center(
-        child: new Column(
-//          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(nepali,
-              style: new TextStyle(fontSize: 48.0)
-            ),
-
-            new Text(english),
-            new ComposeWordOfTheDayWidget(),
-          ],
-        )
+    return new Stack(
+      children: <Widget>[
+        new Center(
+          child: new Column(
+            children: <Widget>[
+              new Text(nepali, style: new TextStyle(fontSize: 48.0)),
+              new Text(english),
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+        ),
+        new Align(
+          child: new ComposeWordOfTheDayWidget(),
+          alignment: Alignment.bottomCenter,
+        ),
+      ],
     );
   }
 }
