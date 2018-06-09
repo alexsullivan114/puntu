bd708787d3ddbf7745dda328305618e547d1cb3a
Alex Sullivan <Alexsullivan114@gmail.com>
2018-06-09 10:02:33 -0400

    Add support for push notifications on Android. iOS to come.

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,24 @@ import 'package:chatty_chat_chat/chat/chat_list_widget.dart';
 import 'package:chatty_chat_chat/chat/chat_widget.dart';
 import 'package:chatty_chat_chat/chat/compose_message_widget.dart';
 import 'package:chatty_chat_chat/home/home_widget.dart';
+import 'package:chatty_chat_chat/users/current_user.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+FirebaseMessaging _firebaseMessaging = new FirebaseMessaging();
 
 void main() => runApp(new MainWidget());
 
-class MainWidget extends StatelessWidget {
+class MainWidget extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  MainWidgetState createState() {
+    return new MainWidgetState();
+  }
+}
+
+class MainWidgetState extends State<MainWidget> {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -20,5 +32,28 @@ class MainWidget extends StatelessWidget {
           body: new HomeWidget(),
         )
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) {
+        print("onMessage: $message");
+      },
+      onLaunch: (Map<String, dynamic> message) {
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) {
+        print("onResume: $message");
+      },
+    );
+
+    _firebaseMessaging.getToken().then((token){
+      final reference = FirebaseDatabase.instance.reference().child('tokens');
+      reference.set({
+        currentUser().index.toString(): token
+      });
+    });
   }
 }
