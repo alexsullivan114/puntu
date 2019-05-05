9aa7cc9f6c746610f725c92e229e948a5a5ee55d
Alex Sullivan <Alexsullivan114@gmail.com>
2019-05-05 10:56:31 -0400

    Add a button to save a translation

--- lib/language/compose_translation.dart
+++ lib/language/compose_translation.dart
@@ -1,20 +1,23 @@
+import 'package:chatty_chat_chat/language/word_of_the_day.dart';
+import 'package:chatty_chat_chat/language/word_of_the_day_streams.dart';
 import 'package:flutter/material.dart';
 
 class ComposeTranslation extends StatelessWidget {
+  final TextEditingController _nepaliController = TextEditingController();
+  final TextEditingController _englishController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(16.0),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Expanded(
             child: TextField(
               expands: true,
-              minLines: null,
               maxLines: null,
               textInputAction: TextInputAction.done,
+              controller: _nepaliController,
               decoration: InputDecoration(
                   labelText: "Nepali",
                   border: OutlineInputBorder(),
@@ -27,13 +30,28 @@ class ComposeTranslation extends StatelessWidget {
             child: TextField(
               textInputAction: TextInputAction.done,
               expands: true,
-              minLines: null,
               maxLines: null,
+              controller: _englishController,
               decoration: InputDecoration(
                   labelText: "English",
                   border: OutlineInputBorder(),
                   contentPadding: EdgeInsets.all(32)),
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 42),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16, bottom: 16),
+            child: SizedBox(
+              width: double.infinity,
+              height: 40,
+              child: RaisedButton(
+                child: Text("Save"),
+                onPressed: () {
+                  addWordOfTheDay(WordOfTheDay(
+                      _nepaliController.text, _englishController.text)
+                  );
+                },
+              ),
             ),
           )
         ],
