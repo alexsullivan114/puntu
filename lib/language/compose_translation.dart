b5d61f182cc731d2d96e7c8a7104985c7e16e9f6
Alex Sullivan <Alexsullivan114@gmail.com>
2019-05-05 14:36:47 -0400

    Add a save button

--- lib/language/compose_translation.dart
+++ lib/language/compose_translation.dart
@@ -2,9 +2,14 @@ import 'package:chatty_chat_chat/language/word_of_the_day.dart';
 import 'package:chatty_chat_chat/language/word_of_the_day_streams.dart';
 import 'package:flutter/material.dart';
 
-class ComposeTranslation extends StatelessWidget {
-  final TextEditingController _nepaliController = TextEditingController();
-  final TextEditingController _englishController = TextEditingController();
+class ComposeTranslation extends StatefulWidget {
+  @override
+  _ComposeTranslationState createState() => _ComposeTranslationState();
+}
+
+class _ComposeTranslationState extends State<ComposeTranslation> {
+  String _nepaliText;
+  String _englishText;
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +22,11 @@ class ComposeTranslation extends StatelessWidget {
               expands: true,
               maxLines: null,
               textInputAction: TextInputAction.done,
-              controller: _nepaliController,
+              onChanged: (newText) {
+                setState(() {
+                  _nepaliText = newText;
+                });
+              },
               decoration: InputDecoration(
                   labelText: "Nepali",
                   border: OutlineInputBorder(),
@@ -31,7 +40,11 @@ class ComposeTranslation extends StatelessWidget {
               textInputAction: TextInputAction.done,
               expands: true,
               maxLines: null,
-              controller: _englishController,
+              onChanged: (newText) {
+                setState(() {
+                  _englishText = newText;
+                });
+              },
               decoration: InputDecoration(
                   labelText: "English",
                   border: OutlineInputBorder(),
@@ -45,17 +58,27 @@ class ComposeTranslation extends StatelessWidget {
               width: double.infinity,
               height: 40,
               child: RaisedButton(
-                child: Text("Save"),
-                onPressed: () {
-                  addWordOfTheDay(WordOfTheDay(
-                      _nepaliController.text, _englishController.text)
-                  );
-                },
+                child: Text(
+                  "Save",
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.blue,
+                onPressed: _savePressed(),
               ),
             ),
           )
         ],
       ),
     );
+  }
+
+  VoidCallback _savePressed() {
+    if (_nepaliText?.isNotEmpty == true && _englishText?.isNotEmpty == true) {
+      return () {
+        addWordOfTheDay(WordOfTheDay(_nepaliText, _englishText));
+      };
+    } else {
+      return null;
+    }
   }
 }
